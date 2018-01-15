a7b63192ace492d63c9fb562e947cd9d5aa79932
Oliver Widder <oliver.widder@gmail.com>
2018-01-15 09:10:15 +0100

    dynamics demo finished

--- demo/dynamic.js/button.js
+++ demo/dynamic.js/button.js
@@ -1,47 +1,49 @@
 /* global dynamics */
 
-var el = document.querySelector('path')
-var textEl = document.querySelector('span')
-var pathOver = el.getAttribute('data-over-path')
-var pathOut = el.getAttribute('d')
+(function () {
+    var el = document.querySelector('path')
+    var textEl = document.querySelector('span')
+    var pathOver = el.getAttribute('data-over-path')
+    var pathOut = el.getAttribute('d')
 
 // Over animation
-function animateOver() {
-    dynamics.animate(el, {
-        d: pathOver,
-        fill: "#007EFF"
-    }, {
-        type: dynamics.spring,
-        complete: animateOut
-    })
+    function animateOver() {
+        dynamics.animate(el, {
+            d: pathOver,
+            fill: "#007EFF"
+        }, {
+            type: dynamics.spring,
+            complete: animateOut
+        })
 
-    dynamics.animate(textEl, {
-        scale: 1.06
-    }, {
-        type: dynamics.spring
-    })
-}
+        dynamics.animate(textEl, {
+            scale: 1.06
+        }, {
+            type: dynamics.spring
+        })
+    }
 
 // Out animation
-function animateOut() {
-    dynamics.animate(el, {
-        d: pathOut,
-        fill: "#0bf"
-    }, {
-        type: dynamics.easeInOut,
-        friction: 100
-    })
+    function animateOut() {
+        dynamics.animate(el, {
+            d: pathOut,
+            fill: "#0bf"
+        }, {
+            type: dynamics.easeInOut,
+            friction: 100
+        })
 
-    dynamics.animate(textEl, {
-        scale: 1
-    }, {
-        type: dynamics.easeInOut,
-        friction: 100,
-        complete: function() {
-            dynamics.setTimeout(animateOver, 500)
-        }
-    })
-}
+        dynamics.animate(textEl, {
+            scale: 1
+        }, {
+            type: dynamics.easeInOut,
+            friction: 100,
+            complete: function() {
+                dynamics.setTimeout(animateOver, 500)
+            }
+        })
+    }
 
 // Start
-animateOver()
+    animateOver()
+})();
