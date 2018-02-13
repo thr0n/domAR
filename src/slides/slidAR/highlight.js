049320b590a4cf4ddc8c2b2fd6236b22b0b882c7
Oliver Widder <oliver.widder@gmail.com>
2018-02-13 08:41:50 +0100

    animated border

--- src/slides/slidAR/highlight.js
+++ src/slides/slidAR/highlight.js
@@ -4,16 +4,35 @@ import * as d3 from 'd3';
 
 const HIGHLIGHT_CLASS = "_highlight_";
 const BORDER_CLASS = "border";
-const LIGHT_CLASS = "light";
+const LIGHT_COLOR_CLASS = "light";
+const DARK_COLOR_CLASS = "dark";
+const ON_CLASS = "on";
+const OFF_CLASS = "off";
 
 const setClass = (selector, className) => {
     d3.selectAll(selector).classed(className, true);
 }
 
-export const lightBorder = (selector) => {
+export const border = (selector, borderColorClass) => {
     setClass(selector, HIGHLIGHT_CLASS);
     setClass(selector, BORDER_CLASS);
-    setClass(selector, LIGHT_CLASS);
+    setClass(selector, borderColorClass);
 }
 
-export const highlight = {lightBorder};
+export const lightBorder = (selector) => {
+    border(selector, LIGHT_COLOR_CLASS)
+}
+
+export const darkBorder = (selector) => {
+    border(selector, DARK_COLOR_CLASS);
+}
+
+export const animatedBorder = (selector, borderColorClass) => {
+    setClass(selector, OFF_CLASS);
+    border(selector, borderColorClass);
+    setTimeout(() => {
+        setClass(selector, ON_CLASS);
+    })
+}
+
+export const highlight = {lightBorder, darkBorder, animatedBorder, LIGHT_COLOR_CLASS, DARK_COLOR_CLASS};
