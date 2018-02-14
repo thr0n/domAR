85c24ba071473a5eb584e5b38bfee2b137cacc17
Oliver Widder <oliver.widder@gmail.com>
2018-02-14 18:41:16 +0100

    popper.css

--- src/slides/slidAR/tooltip.js
+++ src/slides/slidAR/tooltip.js
@@ -1,4 +1,4 @@
-import * as tt from 'tooltip';
+import Tooltip from 'tooltip.js';
 import * as d3 from 'd3';
 
 export const TOP = "top";
@@ -9,7 +9,8 @@ export const LEFT = "left";
 export const create = (title, selector, placement) => {
     d3.selectAll(selector)
         .each(function (_dummy, i) {
-            new tt.Tooltip(this, {title, placement});
+            const instance = new Tooltip(this, {title, placement});
+            instance.show();
         })
 }
 
