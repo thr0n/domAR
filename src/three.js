d2c78f642daa94d45498ef719539f6dbfdbd4041
Oliver Widder <oliver.widder@gmail.com>
2018-02-20 22:26:56 +0100

    removed three.js

--- src/three.js
+++
@@ -1,8 +0,0 @@
-import * as THREE from 'three'
-
-window.THREE = THREE // Not saying it's pretty
-
-require('three/examples/js/controls/TrackballControls.js') // eslint-disable-line
-require('three/examples/js/renderers/CSS3DRenderer.js') // eslint-disable-line
-
-export default THREE
