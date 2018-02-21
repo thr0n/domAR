166d3a809ab678b2447e0ba6b26806a899b0b7fe
Oliver Widder <oliver.widder@gmail.com>
2018-02-21 07:21:47 +0100

    loadHtml

--- src/slides/htmlSlide.js
+++ src/slides/htmlSlide.js
@@ -5,17 +5,7 @@ import * as fct from '../util/fct';
 import {appendScriptsWithReadyFunction} from '../util/loadScript';
 import {slideControl} from './control/SlideControl';
 import {appendStyles} from "../util/loadStyles";
-
-const vsprintf = require('sprintf-js').vsprintf;
-
-export const loadHtmlWithSelector = (selector, pathToHtml) => {
-    log.info(vsprintf("Load html (%s) into selector: %s", [pathToHtml, selector]));
-    $(selector).load(pathToHtml);
-}
-
-const loadHtml = (slideId, pathToHtml) => {
-    loadHtmlWithSelector("#" + slideId, pathToHtml);
-}
+import {loadHtmlWithSelector, loadHtml} from '../util/loadHtml';
 
 const clearHtml = (slideId) => {
     $("#" + slideId).empty();
