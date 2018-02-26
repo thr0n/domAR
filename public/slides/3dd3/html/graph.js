5b79cb6e83cc27c9c35683a7041f0e0488da586d
Oliver Widder <oliver.widder@gmail.com>
2018-02-26 16:55:34 +0100

    svg with dynamics.js

--- public/slides/3dd3/html/graph.js
+++ public/slides/3dd3/html/graph.js
@@ -1,6 +1,9 @@
 /* global slidAR */
 /* global Chart */
 /* global chartColors */
+/* global d3 */
+/* global $ */
+/* global dynamics */
 
 (function () {
     var random0To100 = function() {
@@ -70,6 +73,85 @@
 
     window._graph_clean_donut = function () {
         slidAR.canvas.clearContext(donutContext, 200, 200);
+    }
+
+    window._graph_pin = function () {
+        var svg = d3.selectAll("#graphcube .top")
+            .append("svg")
+            .attr("width", 200)
+            .attr("height", 200)
+            .attr("viewBox", "0 0 200 200")
+
+        var g = svg.append("g")
+            .attr("transform", "translate(50,50)")
+
+        g.append("path")
+            .attr("d", "M38 106.7c2.4 0 37.7-43 37.8-68.8 0-21-17-38-38-38C17 0 0 17 0 38c.4 27 35.4 68.7 38 68.7z")
+            .attr("fill", "#EC1A0A")
+
+        g.append("ellipse")
+            .attr("fill", "#FFF")
+            .attr("cx", "38.1")
+            .attr("cy", "38.1")
+            .attr("rx", "23.1")
+            .attr("ry", "23.1")
+
+        var svg = document.querySelector('#graphcube .top svg')
+        var pin = document.querySelector('#graphcube .top')
+
+        var paused = false;
+
+        function horizontalBounce() {
+            // The bounce animation will return to the original state
+            // In this case, it will go from 0deg to -45deg to 0deg
+            if(!paused) {
+                dynamics.animate(pin, {
+                    rotateZ: -45
+                }, {
+                    type: dynamics.bounce,
+                    duration: 1800,
+                    complete: verticalBounce
+                })
+            }
+        }
+
+        function verticalBounce() {
+            // We animate the two elements (svg, pin) independently
+            dynamics.animate(svg, {
+                scaleY: 0.8
+            }, {
+                type: dynamics.bounce,
+                duration: 800,
+                bounciness: 0
+            })
+
+            // Use the delay option to delay your animations
+            dynamics.animate(pin, {
+                translateY: -60
+            }, {
+                type: dynamics.forceWithGravity,
+                bounciness: 0,
+                duration: 500,
+                delay: 150
+            })
+
+            dynamics.animate(svg, {
+                scaleY: 0.8
+            }, {
+                type: dynamics.bounce,
+                duration: 800,
+                bounciness: 600,
+                delay: 650,
+                complete: horizontalBounce
+            })
+        }
+
+        horizontalBounce()
+
+    }
+
+    window._graph_clean_pin = function () {
+        $("#graphcube .top").empty();
     }
 })()
 
