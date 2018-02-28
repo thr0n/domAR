f3e142d41e84baba2a3a2e49246658d65986c6b1
Oliver Widder <oliver.widder@gmail.com>
2018-02-28 18:21:23 +0100

    temp

--- public/slides/3dd3/js/backAndFront.js
+++ public/slides/3dd3/js/backAndFront.js
@@ -1,14 +1,26 @@
-/* global slidAR, chartColors, Chart */
+/* global slidAR, chartColors, Chart, d3 */
 
 (function () {
     window._backAndFront_pie = function pie() {
-        var canvas = slidAR.canvas.addCanvas("#backAndFront .flipper", 200, 200);
+        var canvas = slidAR.canvas.addCanvas("#backAndFront .flipper", 200, 200, "one");
         var context = canvas.getContext('2d');
 
-        context.fillStyle = "blue";
-        context.fillRect(0, 0, 200, 200);
-
         window._charts_pie(context);
+    }
+
+    window._backAndFront_box = function () {
+        var svg = d3.selectAll("#backAndFront .flipper")
+            .append("svg")
+            .attr("class", "background two")
+            .attr("width", 200)
+            .attr("height", 200)
+            .attr("viewBox", "0 0 200 200")
+            .append("rect")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", 200)
+            .attr("height", 200)
+            .attr("fill", "grey")
     }
 
     window._backAndFront_clean_pie = function _pie() {
